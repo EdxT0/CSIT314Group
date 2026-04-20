48c924ac275dc6914583ae165c6f80b6c59902fa
ZhangYue <zhangyue1380@gmail.com>
2026-04-20 15:01:58 +0800

    Update UpdateFundraiserController.cs

--- CSIT 314 Group/Controllers/Fundraiser/UpdateFundraiserController.cs
+++ CSIT 314 Group/Controllers/Fundraiser/UpdateFundraiserController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CSIT_314_Group.Data;
+using CSIT_314_Group.DTO.FundraiserActivityDTO;
+using CSIT_314_Group.Entity;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CSIT_314_Group.Controllers.FundraiserActivity
@@ -7,5 +10,52 @@ namespace CSIT_314_Group.Controllers.FundraiserActivity
     [ApiController]
     public class UpdateFundraiserController : ControllerBase
     {
+        private readonly FundraiserActivityRepository _fundraiserActivityRepository;
+
+        public UpdateFundraiserController(FundraiserActivityRepository fundraiserActivityRepository)
+        {
+            _fundraiserActivityRepository = fundraiserActivityRepository;
+        }
+
+        [Authorize(Roles = "platform manager")]
+        [HttpPut]
+        public async Task<IActionResult> UpdateFundraiser([FromBody] UpdateFundraiserDTO updateFundraiserDTO)
+        {
+            if (!DateTime.TryParseExact(updateFundraiserDTO.Deadline,
+                    "dd-MM-yyyy",
+                    null,
+                    System.Globalization.DateTimeStyles.None,
+                    out DateTime parsedDeadline))
+            {
+                return BadRequest("Deadline must be in dd-MM-yyyy format");
+            }
+
+            var existingFundraiser = await _fundraiserActivityRepository.GetById(updateFundraiserDTO.Id);
+
+            if (existingFundraiser == null)
+            {
+                return NotFound("Fundraiser not found");
+            }
+
+            var fundraiser = new Fundraiser(
+                updateFundraiserDTO.Name.ToLower(),
+                updateFundraiserDTO.Description,
+                parsedDeadline,
+                updateFundraiserDTO.AmtRequested
+            );
+
+            fundraiser.Id = updateFundraiserDTO.Id;
+            fundraiser.Status = updateFundraiserDTO.Status;
+
+            bool result = await _fundraiserActivityRepository.UpdateFundraiser(fundraiser);
+
+            if (!result)
+            {
+                return StatusCode(500, "Failed to update fundraiser");
+            }
+
+            return Ok($"Updated fundraiser with Id {updateFundraiserDTO.Id}");
+
+        }
     }
 }
