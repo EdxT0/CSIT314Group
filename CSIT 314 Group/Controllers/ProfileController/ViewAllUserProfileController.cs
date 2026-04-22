1522fd210c09a69bd87e205105f8464da909f829
KhongFabian <Fabiankhong@gmail.com>
2026-04-22 12:01:19 +0800

    Added test cases for userProfile cotrollers

--- CSIT 314 Group/Controllers/ProfileController/ViewAllUserProfileController.cs
+++ CSIT 314 Group/Controllers/ProfileController/ViewAllUserProfileController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 namespace CSIT_314_Group.Controllers.ProfileController;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 
 public class ViewAllUserProfileController : ControllerBase
 {
